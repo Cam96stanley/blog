21de055e408a0f59f1896a44436dc3c120f129ba
Cameron Stanley <cam96stanley@gmail.com>
2026-01-17 22:19:38 -0500

    added link around blog card

--- app/components/article-card/ArticleCard.tsx
+++ app/components/article-card/ArticleCard.tsx
@@ -1,11 +1,14 @@
+import Link from "next/link";
 import styles from "./ArticleCard.module.scss";
 
 const ArticleCard = () => {
   return (
-    <article className={styles.article__card}>
-      <h3 className={styles.article__title}>Title</h3>
-      <p className={styles.article__Date}>Date</p>
-    </article>
+    <Link className={styles.blog__link} href="/blog/123">
+      <article className={styles.article__card}>
+        <h3 className={styles.article__title}>Title</h3>
+        <p className={styles.article__Date}>Date</p>
+      </article>
+    </Link>
   );
 };
 
--- app/components/article-card/ArticlePageCard.tsx
+++ app/components/article-card/ArticlePageCard.tsx
@@ -1,12 +1,15 @@
+import Link from "next/link";
 import styles from "./ArticlePageCard.module.scss";
 
 const ArticlePageCard = () => {
   return (
-    <article className={styles.article__card}>
-      <h2 className={styles.article__title}>Blog Title</h2>
-      <p className={styles.article__date}>Article Date</p>
-      <p className={styles.article__body}>Article Body</p>
-    </article>
+    <Link className={styles.blog__link} href="/blog/123">
+      <article className={styles.article__card}>
+        <h2 className={styles.article__title}>Blog Title</h2>
+        <p className={styles.article__date}>Article Date</p>
+        <p className={styles.article__body}>Article Body</p>
+      </article>
+    </Link>
   );
 };
 
